8d498d6de2282aa0740dfa6e9a6bc958e41f1702
MKawcz <s25441@pjwstk.edu.pl>
2022-11-07 21:43:50 +0100

    aplication properties

--- src/main/java/pl/edu/pjwstk/todoapp/controller/TaskController.java
+++ src/main/java/pl/edu/pjwstk/todoapp/controller/TaskController.java
@@ -4,11 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.pjwstk.todoapp.model.Task;
+import pl.edu.pjwstk.todoapp.model.SqlTaskRepository;
 import pl.edu.pjwstk.todoapp.model.TaskRepository;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,6 +20,12 @@ public class TaskController {
 
     public TaskController(TaskRepository repository) {
         this.repository = repository;
+    }
+
+    @PostMapping("/tasks")
+    ResponseEntity<Task> createTask(@RequestBody @Valid Task toCreate) {
+        Task result = repository.save(toCreate);
+        return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 
     @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
@@ -30,6 +38,23 @@ public class TaskController {
     ResponseEntity<List<Task>> readAllTasks(Pageable page){
         logger.info("Custom pageable");
         return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+
+    @GetMapping("/tasks/{id}")
+    ResponseEntity<Task> readTask(@PathVariable long id) {
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?> updateTask(@PathVariable long id, @RequestBody @Valid Task toUpdate) {
+        if(!repository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+        toUpdate.setId(id);
+        repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
     }
 
 }
