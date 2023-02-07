f74c5ba1e7e3f3d36ca2061f77fdd75278021791
MKawcz <s25441@pjwstk.edu.pl>
2023-02-07 21:58:25 +0100

    Field validation in HTML

--- src/main/java/pl/edu/pjwstk/todoapp/controller/ProjectController.java
+++ src/main/java/pl/edu/pjwstk/todoapp/controller/ProjectController.java
@@ -2,26 +2,55 @@ package pl.edu.pjwstk.todoapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.edu.pjwstk.todoapp.logic.ProjectService;
+import pl.edu.pjwstk.todoapp.model.Project;
 import pl.edu.pjwstk.todoapp.model.ProjectStep;
 import pl.edu.pjwstk.todoapp.model.projection.ProjectWriteModel;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+    private final ProjectService service;
+
+    public ProjectController(ProjectService service) {
+        this.service = service;
+    }
+
     @GetMapping
     public String showProjects(Model model) {
         model.addAttribute("project", new ProjectWriteModel());
         return "projects";
     }
 
+    @PostMapping
+    public String addProject(@ModelAttribute("project") @Valid ProjectWriteModel current, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            return "projects";
+        }
+        service.save(current);
+        model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("message", "Dodano projekt!");
+        return "projects";
+    }
+
+
     @PostMapping(params = "addStep")
     public String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
         current.getSteps().add(new ProjectStep());
         return "projects";
+    }
+
+    @ModelAttribute("projects")
+    public List<Project> getProjects() {
+        return service.readAll();
     }
 
 }
